45a1b189d484eb4960376bdca16dfa1d74cefa3c
Ali Parvizi <snparvizi75@gmail.com>
2020-08-08 10:45:24 +0430

    simple input element

--- core/models.py
+++ core/models.py
@@ -26,12 +26,28 @@ class Field(models.Model):
 
 
 class Element(models.Model):
-    """  """
+    """
+    Base class for an input element (defaults to simple text field)
+    value and filters fields may be overridden
+    to cope with other input types
+    """
     title = models.CharField(max_length=255)
     type = models.CharField(max_length=3, choices=element_types, default=INPUT)
 
+    # this field should be set according to the data type
+    # Ex.: a simple text filed may be stored in CharField
+    # Ex.: to store a datetime, value may be DateTimeField
+    value = models.CharField(max_length=1024)
+
     # array of available filters on an element
-    filters = []
+    filters = ['icontains', 'startswith', 'endswith']
+
+    def to_representation(self):
+        """External representation of the value"""
+        pass
+
+    def to_internal_value(self, raw_value):
+        """ convert external(raw) value to internal value """
 
     @property
     def data(self):
