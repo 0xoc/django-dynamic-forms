fb17cdeaac7ffb66d4b20ee2d3f3c1a6243b6b9c
Ali Parvizi <snparvizi75@gmail.com>
2020-08-12 20:44:09 +0430

    bug fix

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('sub-form/<int:sub_form_id>/', RetrieveSubFormView.as_view()),
     path('sub-form/create/', CreateRawSubForm.as_view()),
     path('add-field/', AddFieldToSubForm.as_view()),
-    path('add-element/<element_type>', AddElementToField.as_view())
+    path('add-element/<element_type>/', AddElementToField.as_view())
 
 ]
