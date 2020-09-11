7758be55819342cfceca74e740ca968bc19c10b5
Ali Parvizi <snparvizi75@gmial.com>
2020-09-11 22:41:54 +0430

    disabled element

--- core/serializers/FormSerializers/serializers_headers.py
+++ core/serializers/FormSerializers/serializers_headers.py
@@ -1,4 +1,4 @@
 abstract_base_fields = ['pk', 'title', 'type', 'filters', 'order']
 base_fields = abstract_base_fields + ['value', ]
 base_field_fields = ['pk', 'title', 'elements', 'order', 'is_grid']
-abstract_element_fields = ['pk', 'title', 'type', 'order', 'field', 'data']
+abstract_element_fields = ['pk', 'title', 'type', 'order', 'field', 'data', 'disabled']
