11dde413f0827f98ae181c49a695cb1d07de9b25
Ali Parvizi <snparvizi75@gmail.com>
2020-09-16 17:41:49 +0430

    answer pk

--- core/views/form_views.py
+++ core/views/form_views.py
@@ -357,7 +357,7 @@ class FormFilterView(APIView):
             for answer in answers:
                 answer_data = get_retrieve_serializer(answer.type, simple=True)(instance=answer).data
 
-                _one_element_data['%s_%d' % (answer.type, answer.pk)] = answer_data[answer.value_field]
+                _one_element_data['%s_%d' % (answer.type, answer.answer_of.pk)] = answer_data[answer.value_field]
 
             _elements_data.append(_one_element_data)
             _one_element_data = {}
