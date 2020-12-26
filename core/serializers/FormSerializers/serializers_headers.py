b588793f3da3fde7cd47eb9188725fa8d7c59026
Ali Parvizi <snparvizi75@gmail.com>
2020-12-26 12:51:39 +0330

    condition methods added to field retrieve

--- core/serializers/FormSerializers/serializers_headers.py
+++ core/serializers/FormSerializers/serializers_headers.py
@@ -1,6 +1,10 @@
 abstract_base_fields = ['pk', 'title', 'type', 'filters', 'order']
 base_fields = abstract_base_fields + ['value', ]
-base_field_fields_simple = ['pk', 'title', 'order', 'is_grid']
+base_field_fields_simple = ['pk', 'title', 'order', 'is_grid',
+                            'condition_element_type',
+                            'condition_element_pk',
+                            'condition_element_value',
+                            ]
 base_field_fields = base_field_fields_simple + ['elements', ]
 abstract_element_fields = ['pk', 'title', 'type', 'order',
                            'condition_element_type',
